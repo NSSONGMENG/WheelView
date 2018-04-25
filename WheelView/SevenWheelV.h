af918907d3f79c093b08343a2035d5ac11b3e653
seven.song@ixdigit.com <>
2018-04-25 16:37:13 +0800

    update proj

---
+++ WheelView/SevenWheelV.h
@@ -0,0 +1,27 @@
+//
+//  SevenWheelV.h
+//  FXApp
+//
+//  Created by Seven on 2017/7/17.
+//  Copyright © 2017年 wsz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+/** 轮播图 */
+@interface SevenWheelV : UIView
+
+/** 切换每一页的时间间隔 */
+@property (nonatomic, assign) CGFloat   timeSpace;
+/** 图片url／name */
+@property (nonatomic, strong) NSArray   * items;
+
+
+@end
+
+@interface SevenWheelCell : UICollectionViewCell
+
+/** image url / image name */
+@property (nonatomic, copy) NSString    * imgPath;
+
+@end
